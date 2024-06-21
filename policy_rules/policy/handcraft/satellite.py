5e9f6e92c2b8226df93150e8d6722850d98bab38
Dillon <dillon.chen1@gmail.com>
2024-06-21 17:31:25 +0200

    start satellite

--- policy_rules/policy/handcraft/satellite.py
+++ policy_rules/policy/handcraft/satellite.py
@@ -7,11 +7,6 @@ from ..policy import Policy
 class SatellitePolicy(Policy):
     @override
     def _add_derived_predicates(self):
-        pass
-
-    @override
-    def _add_policy_rules(self):
-        # useful macro
         head = R.instrument_config("I", "M", "S", "D")
         body = [
             R.supports("I", "M"),
@@ -20,63 +15,47 @@ class SatellitePolicy(Policy):
         ]
         self._template += head <= body
 
-        # turn_to
-        turn_to_head = self.relation_from_schema("turn_to")
-        turn_to_rule = self.get_schema_preconditions("turn_to")
-        turn_to_rule += [
+    @override
+    def _add_policy_rules(self):
+        """ turn_to(?s - satellite ?d_new - direction ?d_prev - direction) """
+        body = [
             R.ug_have_image("D_new", "M"),
             R.instrument_config("I", "M", "S", "D_new"),
         ]
-        self._template += turn_to_head <= turn_to_rule
+        self.add_rule("turn_to", body)
 
-        # switch_on
-        switch_on_head = self.relation_from_schema("switch_on")
-        switch_on_rule = self.get_schema_preconditions("switch_on")
-        switch_on_rule += [
+        # for `pointing` goals
+        body = [
+            R.ug_pointing("S", "D_new"),
+            ~R.ug_have_image("D_other", "M_other"),
+            R.instrument_config("I", "M", "S", "D_other"),
+        ]
+        self.add_rule("turn_to", body)
+
+        """ switch_on(?i - instrument ?s - satellite) """
+        body = [
             R.ug_have_image("D", "M"),
             R.pointing("S", "D"),
             R.instrument_config("I", "M", "S", "D"),
             ~R.power_on("I"),
         ]
-        self._template += switch_on_head <= switch_on_rule
+        self.add_rule("switch_on", body)
 
-        # switch_off
+        """ switch_off(?i - instrument ?s - satellite) """
         # TODO
 
-        # calibrate
-        calibrate_head = self.relation_from_schema("calibrate")
-        calibrate_rule = self.get_schema_preconditions("calibrate")
-        calibrate_rule += [
+        """ calibrate(?s - satellite ?i - instrument ?d - direction) """
+        body = [
             R.ug_have_image("D", "M"),
             R.pointing("S", "D"),
             R.instrument_config("I", "M", "S", "D"),
             ~R.calibrated("I"),
         ]
-        self._template += calibrate_head <= calibrate_rule
+        self.add_rule("calibrate", body)
 
-        # take_image
-        take_image_head = self.relation_from_schema("take_image")
-        take_image_rule = self.get_schema_preconditions("take_image")
-        take_image_rule += [
+        """ take_image(?s - satellite ?d - direction ?i - instrument ?m - mode) """
+        body = [
             R.ug_have_image("D", "M"),
             R.instrument_config("I", "M", "S", "D"),
         ]
-        self._template += take_image_head <= take_image_rule
-
-        # helper
-        head = R.exists_unachieved_goal_at("Dir")
-        body = [
-            R.ug_have_image("Dir", "M"),
-        ]
-        self._template += head <= body
-
-        # turn_to
-        # (?s - satellite ?d_new - direction ?d_prev - direction)
-        turn_to_head = self.relation_from_schema("turn_to")
-        turn_to_rule = self.get_schema_preconditions("turn_to")
-        turn_to_rule += [
-            R.ug_pointing("S", "D_new"),
-            ~R.exists_unachieved_goal_at("D_other"),
-            R.instrument_config("I", "M", "S", "D_other"),
-        ]
-        self._template += turn_to_head <= turn_to_rule
+        self.add_rule("take_image", body)
