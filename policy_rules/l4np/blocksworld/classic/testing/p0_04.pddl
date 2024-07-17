aa35cb08e58a828298e57fee21550f448e439d25
Dillon <dillon.chen1@gmail.com>
2024-07-17 14:39:58 +0200

    new benchmarks: use Slaney and Thiebaux's generator for blocksworld as they have opt plans

--- policy_rules/l4np/blocksworld/classic/testing/p0_04.pddl
+++ policy_rules/l4np/blocksworld/classic/testing/p0_04.pddl
@@ -1,24 +1,7 @@
-;; blocks=7, out_folder=testing/easy, instance_id=4, seed=1010
-
-(define (problem blocksworld-04)
- (:domain blocksworld)
- (:objects b1 b2 b3 b4 b5 b6 b7 - object)
- (:init 
-    (arm-empty)
-    (clear b4)
-    (on b4 b3)
-    (on b3 b7)
-    (on b7 b1)
-    (on b1 b2)
-    (on b2 b5)
-    (on b5 b6)
-    (on-table b6))
- (:goal  (and 
-    (clear b4)
-    (on b4 b1)
-    (on b1 b2)
-    (on b2 b3)
-    (on b3 b5)
-    (on b5 b6)
-    (on b6 b7)
-    (on-table b7))))
+;; optimal plan length: 46
+(define (problem blocksworld-n14_r2024)
+    (:domain blocksworld)
+    (:objects b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 - object)
+    (:init (arm-empty) (on-table b1) (on b2 b10) (on b3 b8) (on b4 b1) (on b5 b2) (on b6 b3) (on b7 b12) (on b8 b14) (on b9 b6) (on b10 b13) (on b11 b7) (on b12 b4) (on b13 b9) (on-table b14) (clear b5) (clear b11))
+    (:goal (and (on b1 b13) (on b2 b3) (on b3 b14) (on-table b4) (on b5 b10) (on b6 b5) (on b7 b4) (on b8 b6) (on b9 b7) (on b10 b1) (on b11 b12) (on-table b12) (on b13 b2) (on b14 b11) (clear b8) (clear b9)))
+)
--- results/_results_helper.py
+++ results/_results_helper.py
@@ -47,6 +47,7 @@ for f in sorted(os.listdir("test_logs")):
 df = pd.DataFrame(data)
 df.to_csv("lrnn_results.csv")
 
+
 def group_repeats(df, others_to_keep=None, lrnn=False):
     if others_to_keep is None:
         others_to_keep = []
@@ -67,6 +68,7 @@ def group_repeats(df, others_to_keep=None, lrnn=False):
     df.reset_index(inplace=True)
     return df
 
+
 groups = ["baseline", "scorpion", "lrnn"]
 datas = {}
 for group in groups:
@@ -82,12 +84,14 @@ for group in groups:
 failure_logs = {domain: [] for domain in DOMAINS}
 all_data = pd.concat([data for data in datas.values()])
 easy_problems = set(f"0_{i:02d}" for i in range(1, 31))
+
+
 def plot_domains(metric, log_y=False, easy_only=False):
     for domain in DOMAINS:
         print(domain)
-        data = all_data[all_data['domain'] == domain]
+        data = all_data[all_data["domain"] == domain]
         if easy_only:
-            data = data[data['problem'].isin(easy_problems)]
+            data = data[data["problem"].isin(easy_problems)]
         # failures = data[data.plan_found == False]
         # max_bound_failures = data[data.max_bound == False]
         # failures_not_due_to_max_bound = data[(data.plan_found == False) & (data.max_bound == False)]
@@ -100,5 +104,46 @@ def plot_domains(metric, log_y=False, easy_only=False):
         #     failure_logs[domain].append(f"/pfcalcul/work/dchen/code/cvut-colab/slurm/__experiments/baseline_logs/{row.domain}_{row.problem}_r{row.repeat}.log")
         metric_mean = f"{metric}_mean"
         metric_std = f"{metric}_std"
-        fig = px.scatter(data, x="problem", y=metric_mean, error_y=metric_std, color="solver", log_y=log_y)
+        fig = px.line(data, x="problem", y=metric_mean, error_y=metric_std, color="solver", log_y=log_y)
+        # fig = px.scatter(data, x="problem", y=metric_mean, error_y=metric_std, color="solver", log_y=log_y)
+        fig.show()
+
+
+def plot_difference(absolute=True):
+    for domain in DOMAINS:
+        print(domain)
+        data = all_data[all_data["domain"] == domain]
+
+        data["improvement"] = data.apply(
+            lambda row: -row["plan_length_mean"]
+            + data[
+                (data["domain"] == row["domain"])
+                & (data["problem"] == row["problem"])
+                & (data["solver"] == "baseline")
+            ]["plan_length_mean"].values[0],
+            axis=1,
+        )
+        data["improvement (%)"] = data.apply(
+            lambda row: 100 * (
+                -row["plan_length_mean"]
+                + data[
+                    (data["domain"] == row["domain"])
+                    & (data["problem"] == row["problem"])
+                    & (data["solver"] == "baseline")
+                ]["plan_length_mean"].values[0]
+            )
+            / data[
+                (data["domain"] == row["domain"])
+                & (data["problem"] == row["problem"])
+                & (data["solver"] == "baseline")
+            ]["plan_length_mean"].values[0],
+            axis=1,
+        )
+
+        if absolute:
+            y = "improvement"
+        else:
+            y = "improvement (%)"
+
+        fig = px.line(data, x="problem", y=y, color="solver")
         fig.show()
