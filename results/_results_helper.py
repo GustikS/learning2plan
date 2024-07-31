b0e0e569acea0bf68efe81dd4efb2c5291829570
Dillon <dillon.chen1@gmail.com>
2024-07-31 11:30:37 +0200

    push logs for blocksworld, ferry, satellite; TODO rover

--- results/_results_helper.py
+++ results/_results_helper.py
@@ -439,10 +439,12 @@ if os.path.exists(TRAIN_LOG_DIR):
         train_data["f1"].append(f1)
         train_data["epoch"].append(epoch)
         train_data["time"].append(t)
+        # print(domain, layer, dimension, repeat, loss, f1, epoch, t)
 
     train_df = pd.DataFrame(train_data)
     train_df.to_csv(train_csv_file, index=False)
 else:
+    print("Using existing train csv")
     train_df = pd.read_csv(train_csv_file)
 
 import plotly.graph_objects as go
@@ -452,6 +454,7 @@ from plotly.subplots import make_subplots
 def visualise_train():
     for domain in DOMAINS:
         print(domain)
+        # display(train_df)
         domain_df = train_df[train_df["domain"] == domain]
         domain_df["time"] = domain_df["time"].astype(float)
         fig = make_subplots(rows=1, cols=3)
